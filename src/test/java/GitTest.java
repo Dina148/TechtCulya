664453eb8746b2427d549d5ea476a150b01cd055
dina148 <gulaymamytova21@gmail.com>
2021-03-16 19:00:02 -0500

    for loop

--- src/test/java/GitTest.java
+++ src/test/java/GitTest.java
@@ -1,9 +1,26 @@
+import java.awt.*;
+
 public class GitTest {
+
     public static void main(String[] args) {
         System.out.println(" 100_000.00$");
         System.out.println("Gulya");
         System.out.println("How are you Techtorial students?");
         System.out.println("100");
         System.out.println("I had conflict and i fix it");
+
+        for (int i = 0; i < 10 ; i++) {
+            if(i>0){
+                System.out.println(i);
+            }
+        }
+
+
+
     }
+
+
+
+
+
 }
